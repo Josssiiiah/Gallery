8e2daec93b5e695b7a5509a50853295c3f980c31
Josiah Griggs <josiahstanford8@gmail.com>
2024-04-22 00:02:28 -0700

    D1 set up properly

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,7 @@
 export default function Index() {
-  
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div>
       <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
--- app/routes/app/route.tsx
+++ app/routes/app/route.tsx
@@ -1,48 +1,33 @@
 // File: app/routes/api/beverages.tsx
 
-import { LoaderFunction, json } from '@remix-run/cloudflare';
-import { useLoaderData } from '@remix-run/react';
+import { LoaderFunction, json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
-// Assuming a type for the fetched data, adjust according to your actual data schema
 type Beverage = {
   CompanyName: string;
   ContactName: string;
 };
 
-// Define the environment interface if it's not globally available
-interface Env {
-  DB: D1Database;
-}
-
-// Loader function to fetch data
 export const loader: LoaderFunction = async ({ context }: any) => {
-  const db: D1Database = context.DB; // Make sure DB is passed in the context
-     const { env } = context.cloudflare;
-    let { results } = await env.DB.prepare(
+  const db: D1Database = context.DB;
+
+  const { env } = context.cloudflare;
+  let { results } = await env.DB.prepare(
     "SELECT CompanyName, ContactName FROM Customers WHERE CompanyName = ?"
   )
     .bind("Bs Beverages")
     .all();
 
-  return json(results); // Return results as JSON
+  console.log("results", results);
+  return json(results);
 };
 
-// Component to render the fetched data
-const Beverages: React.FC = () => {
+export default function Route() {
   const beverages = useLoaderData<Beverage[]>();
 
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.5' }}>
-      <h1>Beverages Customers</h1>
-      <ul>
-        {beverages.map((beverage: any, index: any) => (
-          <li key={index}>
-            {beverage.CompanyName} - {beverage.ContactName}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
-};
-
-export default Beverages;
+}
